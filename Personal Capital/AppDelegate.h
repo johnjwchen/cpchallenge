4f42c895d5e84676e6ed19d14a26ba37155a5e89
JIAWEI CHEN <jiaweichen@JIAWEIs-MacBook-Pro.local>
2017-08-06 17:28:26 -0700

    delete storyboard and add code to manually create window & root view controller.

---
+++ Personal Capital/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Personal Capital
+//
+//  Created by JIAWEI CHEN on 8/6/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Personal Capital/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Personal Capital
+//
+//  Created by JIAWEI CHEN on 8/6/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
